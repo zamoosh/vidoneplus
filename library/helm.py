f1666f95c51ad9ee0ab0b4554a7686ddc4815edc
mh.eftekhari <mh.eftekhari@gmail.com>
2021-10-24 19:32:37 +0330

    install vidone site helm configurations

--- library/helm.py
+++ library/helm.py
@@ -7,13 +7,33 @@ class Helm:
     def __init__(self):
         self.pull_helm_chart()
         self.HELM = os.path.join(settings.BASE_DIR, 'library', 'helm')
+        self.CHART_VIDONE_SITE = os.path.join(settings.BASE_DIR, 'helm-chart', 'website')
         self.HELM_CONFIG = '--kubeconfig=' + os.path.join(settings.BASE_DIR, 'library', 'config')
 
     def pull_helm_chart(self):
         subprocess.run(['git', 'clone', 'https://gitlab.vps-vds.com/vidone/helm-chart'], stdout=subprocess.PIPE)
 
-    def delete_app(self):
-        return self.HELM
+    def delete_app(self, appname):
+        subprocess.run([self.HELM, self.HELM_CONFIG, 'delete', appname], stdout=subprocess.PIPE)
+
+    def install_app(self, appname):
+        params = [self.HELM, self.HELM_CONFIG, 'upgrade', '--install', appname, self.CHART_VIDONE_SITE]
+        params += ['--set', "database.dbengine='django.db.backends.mysql'"]
+        params += ['--set', "database.dbname='vidone_website_stage'"]
+        params += ['--set', "database.dbuser='vidone_website_stage'"]
+        params += ['--set', "database.dbpassword='9!JE[Ht^)h;5'"]
+        params += ['--set', "database.dbhost='cpanel.vidone.org'"]
+        params += ['--set', "ingress.hosts[0].host='app.vidone.org'"]
+        params += ['--set', 'ingress.hosts[0].paths[0]="/"']
+        params += ['--set', 'ingress.tls[0].secretName=app-vidone-cert']
+        params += ['--set', 'nameOverride="vidone-stage"']
+        params += ['--set', 'fullnameOverride="vidone-stage"']
+        params += ['--set', 'image.tag=0.0.0-beta45']
+        try:
+            subprocess.run(params, stdout=subprocess.PIPE)
+            return True
+        except:
+            return False
 
     def get_installed_list(self):
         result = subprocess.run([self.HELM, self.HELM_CONFIG, 'list'], stdout=subprocess.PIPE)
