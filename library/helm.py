f47887eb3a5dda2ee7c04a8acd4983ebf76cbd43
mh.eftekhari <mh.eftekhari@gmail.com>
2021-10-25 16:03:09 +0330

    helm full configured

--- library/helm.py
+++ library/helm.py
@@ -7,7 +7,7 @@ class Helm:
     def __init__(self):
         self.pull_helm_chart()
         self.HELM = os.path.join(settings.BASE_DIR, 'library', 'helm')
-        self.CHART_VIDONE_SITE = os.path.join(settings.BASE_DIR, 'helm-chart', 'website')
+        self.HELM_CHART = os.path.join(settings.BASE_DIR, 'helm-chart')
         self.HELM_CONFIG = '--kubeconfig=' + os.path.join(settings.BASE_DIR, 'library', 'config')
 
     def pull_helm_chart(self):
@@ -19,11 +19,19 @@ class Helm:
     def delete_app(self, appname):
         subprocess.run([self.HELM, self.HELM_CONFIG, 'delete', appname], stdout=subprocess.PIPE)
 
-    def install_app(self, appname, path, tagimage):
+    def install_app(self, prj, appname, path, tagimage):
+        '''
+            this library install image docker on Kubernetes
+        :param prj: String field [admindashvidone,frontvidone,website]
+        :param appname: name app to install on kubernetes
+        :param path: yaml file path
+        :param tagimage: image tag name
+        :return: True or False
+        '''
         try:
             subprocess.run(
-                [self.HELM, self.HELM_CONFIG, 'upgrade', '--install', appname, self.CHART_VIDONE_SITE, '-f', path,
-                 '--set', 'image.tag=' + tagimage],
+                [self.HELM, self.HELM_CONFIG, 'upgrade', '--install', appname,
+                 os.path.join(self.self.HELM_CHART, prj), '-f', path, '--set', 'image.tag=' + tagimage],
                 stdout=subprocess.PIPE)
             return True
         except:
