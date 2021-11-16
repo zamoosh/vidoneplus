47f3f98ff1bd41dc494d0d18aec6e77efb375d9e
mh.eftekhari <mh.eftekhari@gmail.com>
2021-11-16 17:40:21 +0330

    fix bugs

--- client/views/__init__.py
+++ client/views/__init__.py
@@ -9,4 +9,4 @@ from .profile import profile
 from .setting import user_settings, configs
 from .verify import verify
 from .changepassword import changepassword
-from .installation import admin, admininstall, adminremove, check_or_createuser, resetpassword, install_sites
+from .installation import admin, admininstall, adminremove, check_or_createuser, resetpassword, install_sites, verion, edit_verion, create_verion
