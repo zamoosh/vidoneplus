1c5028dda2cf4bb9e8da524e92de6432b770c40f
Maryam Teimouri <maryam.teimouri.bd.com>
2022-01-15 22:45:39 +0330

    changepass.py and imports.py in view in client changed changes: 	dictionary instead of elifs 	inner loops improved

--- client/views/changepassword.py
+++ client/views/changepassword.py
@@ -4,17 +4,16 @@ from .imports import *
 def changepassword(request):
     context = {}
     if request.method == "POST":
-        if request.POST.get("newpassword", None) and request.POST.get("password", None):
-            if authenticate(username=request.user.username, password=request.POST.get("password", None)):
-                if pwStrength(request.POST.get("newpassword", "")) > 60:
-                    user = request.user
-                    user.set_password(request.POST["newpassword"])
-                    user.save()
-                    context['result'] = "گذر واژه با موفقیت تغییر کرد."
-                else:
-                    context['error'] = "گذر واژه ی جدید آسان می باشد."
-            else:
-                context['error'] = "گذرواژه صحیح نیست."
-        else:
+        if not request.POST.get("newpassword", None) or not request.POST.get("password", None):
             context['error'] = "گذرواژه صحیح نیست."
+        elif not authenticate(username=request.user.username, password=request.POST.get("password", None)):
+            context['error'] = "گذرواژه صحیح نیست."
+        elif pwStrength(request.POST.get("newpassword", "")) <= 60:
+            context['error'] = "گذر واژه ی جدید آسان می باشد."
+        else:
+            user = request.user
+            user.set_password(request.POST["newpassword"])
+            user.save()
+            context['result'] = "گذر واژه با موفقیت تغییر کرد."
+            
     return render(request, "client/profile_change_password.html", context)
